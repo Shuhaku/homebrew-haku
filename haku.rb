824d60c76f3e2bcc352dc6a641ceaed17026ffc7
Shuhaku <Shuhaku7@icloud.com>
2024-11-28 23:16:08 +0900

    Add formula and tar

---
+++ haku.rb
@@ -0,0 +1,17 @@
+class Haku < Formula
+  desc "Prints the given arguments to the command line"
+  homepage "https://github.com/shuhaku/haku"
+  url "https://github.com/yourusername/haku/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "69896c5a8bf0c8697d5bdda95fd276da509a354f9cfc5f0a4f84c055b83bea57"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    bin.install "haku"
+  end
+
+  test do
+    assert_equal "insun\n", shell_output("#{bin}/haku insun").strip
+  end
+end
