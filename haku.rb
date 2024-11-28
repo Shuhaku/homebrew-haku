b4b6550641e160f949a26d233d6cf7b3fa39e50f
Shuhaku <Shuhaku7@icloud.com>
2024-11-28 23:57:54 +0900

    Update haku formula to v.1.0.1

--- haku.rb
+++ haku.rb
@@ -1,8 +1,8 @@
 class Haku < Formula
   desc "Prints the given arguments to the command line"
   homepage "https://github.com/shuhaku/haku"
-  url "https://github.com/shuhaku/haku/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "69896c5a8bf0c8697d5bdda95fd276da509a354f9cfc5f0a4f84c055b83bea57"
+  url "https://github.com/shuhaku/haku/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "dbd33034a018d697ac75137df239c2a58a1d2ee4ad707de2df73e700a1100661"
   license "MIT"
 
   depends_on "node"
