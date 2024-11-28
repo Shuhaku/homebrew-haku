607825c8d6876a23a0e61b7fa95f7e973bf1351d
Shuhaku <Shuhaku7@icloud.com>
2024-11-28 23:17:11 +0900

    Edit formula

--- haku.rb
+++ haku.rb
@@ -1,7 +1,7 @@
 class Haku < Formula
   desc "Prints the given arguments to the command line"
   homepage "https://github.com/shuhaku/haku"
-  url "https://github.com/yourusername/haku/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/shuhaku/haku/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "69896c5a8bf0c8697d5bdda95fd276da509a354f9cfc5f0a4f84c055b83bea57"
   license "MIT"
 
