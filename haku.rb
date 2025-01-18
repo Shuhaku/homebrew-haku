edf29a6614e6e78578e4a975db33a63ffe1c908b
Shuhaku <Shuhaku7@icloud.com>
2025-01-18 23:18:02 +0900

    chore: update Haku to v1.0.2

--- haku.rb
+++ haku.rb
@@ -1,17 +1,19 @@
 class Haku < Formula
-  desc "Prints the given arguments to the command line"
+  desc "A CLI tool for text formatting"
   homepage "https://github.com/shuhaku/haku"
-  url "https://github.com/shuhaku/haku/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "dbd33034a018d697ac75137df239c2a58a1d2ee4ad707de2df73e700a1100661"
+  url "https://github.com/shuhaku/haku/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "db883236a08ef720ab32486d2802ce5858e34f7056279bc6924ac3e5ac1ec56c"
   license "MIT"
 
   depends_on "node"
 
   def install
-    bin.install "haku"
+    system "npm", "install"
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"bin/main.js" => "haku"
   end
 
   test do
-    assert_equal "insun\n", shell_output("#{bin}/haku insun").strip
+    assert_match "helloWorld", shell_output("#{bin}/haku -c hello world")
   end
 end
